00e21bd5407433389d15788cb12eb02906f1dfa2
Oleksandr Kovalchuk <anxolerd@outlook.com>
2016-10-05 01:23:19 +0300

    Modify init sql
    - Create tables if they do not exist
    - Create constraints if they do not exists

--- core/src/main/java/edu/anxolerd/inquisition/core/DbHelper.java
+++ core/src/main/java/edu/anxolerd/inquisition/core/DbHelper.java
@@ -4,13 +4,13 @@ package edu.anxolerd.inquisition.core;
 public class DbHelper {
     public static final String INIT_SQL = (
         "CREATE EXTENSION IF NOT EXISTS pgcrypto;\n" +
-        "CREATE TABLE interest (\n" +
+        "CREATE TABLE IF NOT EXISTS interest (\n" +
         "  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),\n" +
         "  title TEXT NOT NULL,\n" +
         "  description TEXT,\n" +
         "  sin_rate INTEGER NOT NULL\n" +
         ");\n" +
-        "CREATE TABLE person (\n" +
+        "CREATE TABLE IF NOT EXISTS person (\n" +
         "  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),\n" +
         "  first_name TEXT NOT NULL,\n" +
         "  middle_name TEXT,\n" +
@@ -18,15 +18,15 @@ public class DbHelper {
         "  birth_date DATE NOT NULL,\n" +
         "  death_date DATE\n" +
         ");\n" +
-        "CREATE TABLE m2m_person_interest (" +
+        "CREATE TABLE IF NOT EXISTS m2m_person_interest (" +
         "  person_id UUID NOT NULL,\n" +
         "  interest_id UUID NOT NULL\n" +
         ");\n" +
-        "ALTER TABLE m2m_person_interest ADD CONSTRAINT m2m_person_interest_person_id_fk\n" +
+        "ALTER TABLE m2m_person_interest ADD CONSTRAINT IF NOT EXISTS m2m_person_interest_person_id_fk\n" +
         "  FOREIGN KEY (person_id) REFERENCES person(id) ON DELETE CASCADE;\n" +
-        "ALTER TABLE m2m_person_interest ADD CONSTRAINT m2m_person_interest_interest_id_fk\n" +
+        "ALTER TABLE m2m_person_interest ADD CONSTRAINT IF NOT EXISTS m2m_person_interest_interest_id_fk\n" +
         "  FOREIGN KEY (interest_id) REFERENCES interest(id) ON DELETE CASCADE;\n" +
-        "ALTER TABLE m2m_person_interest ADD CONSTRAINT m2m_person_interest_person_id_interest_id_ix\n" +
+        "ALTER TABLE m2m_person_interest ADD CONSTRAINT IF NOT EXISTS m2m_person_interest_person_id_interest_id_ix\n" +
         "  UNIQUE (person_id, interest_id);\n" +
         "CREATE OR REPLACE FUNCTION calc_sin_rate(person_id UUID) RETURNS INTEGER AS $$\n" +
         "  SELECT " +
@@ -35,7 +35,7 @@ public class DbHelper {
         "    JOIN interest ON m2m_person_interest.interest_id = interest.id\n" +
         "  WHERE m2m_person_interest.person_id = $1;\n" +
         "$$ LANGUAGE 'sql';\n" +
-        "CREATE TABLE \"case\" (\n" +
+        "CREATE TABLE IF NOT EXISTS \"case\" (\n" +
         "  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),\n" +
         "  suspect_person_id UUID NOT NULL,\n" +
         "  date_opened TIMESTAMP WITHOUT TIME ZONE NOT NULL,\n" +
@@ -45,7 +45,7 @@ public class DbHelper {
         "  verdict INTEGER,\n" +
         "  sentence TEXT\n" +
         ");\n" +
-        "ALTER TABLE \"case\" ADD CONSTRAINT case_suspect_person_id_fk " +
+        "ALTER TABLE \"case\" ADD CONSTRAINT IF NOT EXISTS case_suspect_person_id_fk " +
         "  FOREIGN KEY (suspect_person_id) REFERENCES person(id);\n"
     );
 
