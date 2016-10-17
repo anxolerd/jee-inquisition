fbd20a043c288fc23af175176b0e0f635bef1a38
Oleksandr Kovalchuk <anxolerd@outlook.com>
2016-10-18 02:12:13 +0300

    Cleanup InterestService.java

--- jpajsf/src/main/java/edu/anxolerd/inquisition/services/InterestService.java
+++ jpajsf/src/main/java/edu/anxolerd/inquisition/services/InterestService.java
@@ -6,7 +6,7 @@ import edu.anxolerd.inquisition.jpa.entities.Interest;
 import edu.anxolerd.inquisition.jpa.session.InterestFacade;
 
 import javax.faces.bean.ManagedBean;
-import javax.persistence.EntityTransaction;
+
 
 @ManagedBean(name = "InterestService")
 public class InterestService {
@@ -21,16 +21,13 @@ public class InterestService {
             .setSinRate(20);
 
         InterestFacade interestFacade = new InterestFacade();
-//        EntityTransaction tx = PersistenceUtil.getEntityManager().getTransaction();
         try {
             PersistenceUtil.getEntityManager().getTransaction().begin();
-    //        tx.begin();
             interestFacade.create(interest);
             PersistenceUtil.getEntityManager().getTransaction().commit();
-        } catch (Exception e){
+        } catch (Exception e) {
             PersistenceUtil.getEntityManager().getTransaction().rollback();
             throw e;
         }
-//        tx.commit();
     }
 }
